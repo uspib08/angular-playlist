c6234fb359aa2c350c4febd091b985aa901bea98
AzureAD\AlexisCampan <alexis.campan@toulouse.miage.fr>
2021-03-01 22:58:39 +0100

    co

--- ProjetWebNode/ServeurApplication.js
+++ ProjetWebNode/ServeurApplication.js
@@ -7,6 +7,15 @@ let b1 = new banquePlaylist();
 
 app.use(express.json())
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.post('/creerPlay/:nom/:createur/:style', function(req,res) {
     // var objres = b1.creerPlaylist(req.params.nom, req.params.createur, req.params.style);
     b1.creerPlaylist(req.params.nom, req.params.createur, req.params.style);
