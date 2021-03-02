781bef2c119a4162ac590b4c16b8349347f81ee6
AzureAD\AlexisCampan <alexis.campan@toulouse.miage.fr>
2021-03-02 22:30:57 +0100

    u

--- ProjetWeb/src/app/Lister-Playlist/Lister-Playlist.component.ts
+++ ProjetWeb/src/app/Lister-Playlist/Lister-Playlist.component.ts
@@ -22,10 +22,14 @@ export class ListerPlaylistComponent implements OnInit {
   ngOnInit() {
   }
 
-   public afficherListPlaylist(){
-     this.apimusique.afficherToutePlaylist().subscribe((response) => {this.listPlaylist.push(response)},
-    (error)=>{console.log("Erreur d'affichage playlist : " +error)});
-    console.log(this.listPlaylist);
+
+  public afficherListPlaylist(){
+    this.apimusique.afficherToutePlaylist().subscribe((response) => {this.listPlaylist.push(response)},
+   (error)=>{console.log("Erreur d'affichage playlist : " +error)});
+   console.log(this.listPlaylist);
+  //  for(let p in this.listPlaylist){
+  //    console.log(this.listPlaylist[p]._nom);
+  //  }
 
 
    }
