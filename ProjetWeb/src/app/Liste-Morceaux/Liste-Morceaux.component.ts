d4692adac44f9b51e85677c56f085105fe385398
AzureAD\AlexisCampan <alexis.campan@toulouse.miage.fr>
2021-03-06 15:41:34 +0100

    commit du futur

--- ProjetWeb/src/app/Liste-Morceaux/Liste-Morceaux.component.ts
+++ ProjetWeb/src/app/Liste-Morceaux/Liste-Morceaux.component.ts
@@ -1,5 +1,6 @@
+import { ApiMusiqueService } from './../apiMusique.service';
 import { Morceau } from './../Morceau';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostBinding, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-Liste-Morceaux',
@@ -11,21 +12,25 @@ export class ListeMorceauxComponent implements OnInit {
   public liste: any;
   public m: Morceau = new Morceau();
 
-  constructor() {
+
+  constructor(private apimusique:ApiMusiqueService) {
     this.liste = new Array();
   }
 
   ngOnInit() {
-
-
-
   }
 
-  ajouter(artiste:string, titre:string){
+
+  ajouter(artiste:string, titre:string): void{
     const m2 = new Morceau();
     m2._artiste = artiste;
     m2._titre = titre
     this.liste.push(m2);
+  }
+  envoyer(tit:string, art:string){
+    console.log(tit+"/"+art);
+
+    this.apimusique.ajouterMorceau(0,tit,art);
   }
 
 }
